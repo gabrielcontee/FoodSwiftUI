889666ecf4a96be32c183a71a3da34c8246e71c1
x159469 <luan.nascimento.almeida@everis.com>
2020-07-26 12:07:02 -0300

    Created Xcode Project

---
+++ FoodSwiftUI/FoodSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodSwiftUI
+//
+//  Created by Gabriel Figueiredo Conde on 7/25/20.
+//  Copyright © 2020 Gabriel Figueiredo Conte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
